518cdd4b60e8731dbefa70ecbd70d39d013c2c19
jayaganeshkumar <56192588+jayaganeshkumar@users.noreply.github.com>
2022-04-18 12:31:15 +0530

    add movie data

---
+++ netlix_app/src/cdata/index.js
@@ -0,0 +1,27 @@
+const cc=[
+    {
+        imgsrc: 'https://dnm.nflximg.net/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVi07erihcYzF5tEDSAMtg9nsFAV3qX6BmOyuYODr5569Ym2ZezmcKYN0YY2ODXs_ZjQiL8PjCCKZSVmKCnefDW4EOEbFbj1L9zcfH-ZKN2Z7LQYpuWHFlgeTGofHg.jpg?r=3db',
+        title: 'The Adam Project',
+        sname: 'TAP',
+        link: 'https://www.netflix.com/in/title/81309354',
+    },
+    {
+        imgsrc: 'https://images.immediate.co.uk/remote/m.media-amazon.com/images/M/MV5BY2E4MmNmMzgtOTI2YS00MzhjLTlmOWItZTkxNWRkYTY2NDcwXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg',
+        title: 'NightBooks',
+        sname: 'NB',
+        link: 'https://www.netflix.com/in/title/81002270'
+    },
+    {
+        imgsrc: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/over-the-moon-netflix-movie-1604866047.png',
+        title: 'Over The Moon',
+        sname: 'OTM',
+        link: 'https://www.netflix.com/in/title/80214236'
+    },
+    {
+        imgsrc:'http://img.wynk.in/unsafe/320x180/top/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/music/Middle-Class-Melodies-720x405.png',
+        title: 'Middle Class Melodies',
+        sname: 'MCL',
+        link: 'https://www.amazon.com/Middle-Class-Melodies-Anand-Deverakonda/dp/B08NV311YB'
+    }
+]
+export default cc;
